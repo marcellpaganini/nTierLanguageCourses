d6d3d8ac5465d7cfd5bc8a811cb6539e8992cd3d
marcellpaganini <59565705+marcellpaganini@users.noreply.github.com>
2020-12-31 18:01:44 -0400

    Repo/Service/References

---
+++ Project/LanguageCourses/Repo/StudentRepo.cs
@@ -0,0 +1,19 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repo
+{
+    public class StudentRepo
+    {
+        private DataAccess db;
+
+        public StudentRepo()
+        {
+            db = new DataAccess();
+        }
+    }
+}
---
+++ Project/LanguageCourses/Service/StudentService.cs
@@ -0,0 +1,19 @@
+﻿using Repo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class StudentService
+    {
+        private StudentRepo repo;
+
+        public StudentService()
+        {
+            repo = new StudentRepo();
+        }
+    }
+}
